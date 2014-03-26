265ca8b9f7172f6ba21e60c6497e0af84a8ca69b
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-03-26 12:16:11 +0000

    [fix] load, parse and print

--- Align.py
+++ Align.py
@@ -1,5 +1,3 @@
-
-		
 class Sequence:
 	def __init__(self, seq):
 		self.seq = seq
@@ -8,9 +6,15 @@ class Sequence:
 	def load(filename):
 		f = open(filename)
 		sequences = []
+		seq = ""
 		for line in f:
-			if line[0] != '>':
-				sequences.append(Sequence(line[:-1]))
+			if line[0].isalpha():
+				seq += line[:-1]
+			else:
+				sequences.append(Sequence(seq))
+				seq = ""
+		if (seq != ""):
+			sequences.append(Sequence(seq))
 		f.close()
 		return sequences
 	
@@ -43,10 +47,20 @@ class Score:
 	def __repr__(self):
 		ret = ""
 		for char in self.acides:
-			ret += char + ' '
+			ret += ' ' + char + ' '
 		ret += '\n'
 		for line in self.matrice:
 			for char in line:
+				if (char >= 0):
+					ret += ' '
 				ret += str(char) + ' '
 			ret += '\n'
 		return ret
+
+
+sequences = Sequence.load("PDZ-sequences.fasta")
+for seq in sequences:
+	print(seq)
+
+score = Score.load("blosum80.txt")
+print(score)
--- test.py
+++
@@ -1,8 +0,0 @@
-from Align import *
-
-sequences = Sequence.load("maguk-sequences.fasta")
-for seq in sequences:
-	print(seq)
-
-score = Score.load("blosum80.txt")
-print(score)
