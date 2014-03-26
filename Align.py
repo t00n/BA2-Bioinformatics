b68392f797ae180a042869f7502f7023dc5be356
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-03-26 17:34:07 +0000

    [add] Align now find one global alignment

--- Align.py
+++ Align.py
@@ -2,7 +2,6 @@ class Sequence:
 	def __init__(self, seq):
 		self.seq = seq
 		
-	@staticmethod
 	def load(filename):
 		f = open(filename)
 		sequences = []
@@ -24,27 +23,51 @@ class Sequence:
 
 	def __repr__(self):
 		return self.seq
-	
+
+	def __len__(self):
+		return len(self.seq)
+
+	def alignEnd(self, other, score, penalty, matrix, i, j, alignmentA, alignmentB):
+		while (i > 0 or j > 0):
+			if (i > 0 and j > 0 and matrix[i][j] == matrix[i-1][j-1] + score[self[j], other[i]]):
+				alignmentA = self[j] + alignmentA
+				alignmentB = other[i] + alignmentB
+				i -= 1
+				j -= 1
+			elif (j > 0 and matrix[i][j] == matrix[i][j-1] + penalty):
+				alignmentA = self[j] + alignmentA
+				alignmentB = "-" + alignmentB
+				j -= 1
+			elif (i > 0 and matrix[i][j] == matrix[i-1][j] + penalty):
+				alignmentA = "-" + alignmentA
+				alignmentB = other[i] + alignmentB
+				i -= 1
+		print(self[0] + alignmentA)
+		print(other[0] + alignmentB)
+
 	def align(self, other, score, penalty):
-		matrix = [x[:] for x in [[0]*len(self.seq)]*len(other.seq)]
-		for i in range(0, len(self.seq)):
+		matrix = [x[:] for x in [[0]*len(self)]*len(other)]
+		for i in range(0, len(self)):
 			matrix[0][i] = penalty*i
-		for i in range(0, len(other.seq)):
+		for i in range(0, len(other)):
 			matrix[i][0] = penalty*i
-		for i in range(1, len(other.seq)):
-			for j in range(1, len(self.seq)):
-				#print(str(i)+":"+str(j)+":"+str(matrix[i][j])+":"+str(self[j])+":"+str(other[i])+":"+str(score[self[j], other[i]]))
+		for i in range(1, len(other)):
+			for j in range(1, len(self)):
 				match = matrix[i-1][j-1] + score[self[j], other[i]]
 				delete = matrix[i-1][j] + penalty
 				insert = matrix[i][j-1] + penalty
 				matrix[i][j] = max(match, delete, insert)
+		alignmentA = ""
+		alignmentB = ""
+		i = len(other) - 1
+		j = len(self) - 1
+		self.alignEnd(other, score, penalty, matrix, i, j, alignmentA, alignmentB)
 
 class Score:
-	def __init__(self, matrice, acides):
+	def __init__(self, matrice, indexes):
 		self.matrice = matrice
-		self.acides = acides
+		self.indexes = indexes
 	
-	@staticmethod
 	def load(filename):
 		f = open(filename)
 		matrice = []
@@ -52,21 +75,21 @@ class Score:
 			if line[0] == '#':
 				pass
 			elif line[0] == ' ':
-				acides = line.split()
+				indexes = line.split()
 			else:
 				l = line[1:]
 				matrice.append(list(map(int, l.split())))
 		f.close()
-		return Score(matrice, acides)
+		return Score(matrice, indexes)
 		
-	def __getitem__(self, index):
-		i = self.acides.index(index[0])
-		j = self.acides.index(index[1])
+	def __getitem__(self, acide):
+		j = self.indexes.index(acide[0]) # get index of letter acide[0]
+		i = self.indexes.index(acide[1]) # get index of letter acide[1]
 		return self.matrice[i][j]
 
 	def __repr__(self):
 		ret = ""
-		for char in self.acides:
+		for char in self.indexes:
 			ret += ' ' + char + ' '
 		ret += '\n'
 		for line in self.matrice:
@@ -78,9 +101,9 @@ class Score:
 		return ret
 
 
-sequences = Sequence.load("PDZ-sequences.fasta")
-maguk = Sequence.load("maguk-sequences.fasta")
+pdz = Sequence.load("PDZ-sequences.fasta")
 score = Score.load("blosum80.txt")
+# maguk = Sequence.load("maguk-sequences.fasta")
 
 # for seq in sequences:
 # 	print(seq)
@@ -90,4 +113,5 @@ score = Score.load("blosum80.txt")
 
 # print(score)
 
-sequences[0].align(sequences[1], score, -4)
+
+pdz[0].align(pdz[1], score, -4)
