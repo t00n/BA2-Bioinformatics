bc0e2b90a586140d758637b97240abbb50c9e1bd
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-04-15 15:08:35 +0100

    YESSSSS

--- Blosum.py
+++ Blosum.py
@@ -1,50 +1,5 @@
 from Sequence import Sequence
 
-class Cluster:
-	def __init__(self, sequences, identity):
-		self.identity = identity
-		self.sequences = []
-
-		# self.matrix = [x[:] for x in [[False]*len(sequences)]*len(sequences)]
-		# for i in range(0, len(sequences)):
-		# 	for j in range(0, len(sequences)):
-		# 		self.matrix[i][j] = sequences[i].identity(sequences[j]) >= identity
-
-	def __getitem__(self, index):
-		return self.sequences[index]
-
-	def __repr__(self):
-		ret = ""
-		for seq in self.sequences:
-			ret += seq.__repr__() + "\n"
-		return ret
-
-	def match(self, i):
-		pass
-
-
-
-	def build(self, sequences, i):
-		if (sequences[i] not in self.sequences):
-			self.sequences.append(sequences[i])
-			match = False
-			index = -1
-			for j in range(i+1, len(sequences)):
-				print(str(self.sequences)+":"+str(match)+":"+str(i)+":"+str(j)+":"+str(index))
-				if (match):
-					if (sequences[index].identity(sequences[j]) >= self.identity):
-						self.build(sequences, j)
-				elif (sequences[i].identity(sequences[j]) >= self.identity):
-					match = True
-					index = j
-			self.build(sequences, index)
-
-
-class Blosum:
-	def __init__(self, sequences):
-		pass
-
-
 def distance(cluster1, cluster2):
 	result = 0
 	for i in range(0, len(cluster1)):
@@ -52,19 +7,8 @@ def distance(cluster1, cluster2):
 			result += cluster1[i].identity(cluster2[j]) >= 50 # or 38 or 60
 	return 1-(result/(len(cluster1)*len(cluster2)))
 
-def global_dissim(clusters):
-	result = True
-	for cluster in clusters:
-		tmp_result = 0
-		for seq1 in cluster:
-			for seq2 in cluster:
-				tmp_result = max(seq1.distance(seq2), tmp_result)
-
-		result = tmp_result <= 50 and result
-	return result
-
 if __name__ == '__main__':
-	sequences = [ Sequence("ATCKQ"), Sequence("ATCRN"), Sequence("ASCKN"), Sequence("SSCRN"), Sequence("SDCEQ"), Sequence("SECEN"), Sequence("TECRQ") ]
+	sequences = [ Sequence("TECRQ"), Sequence("SSCRN"), Sequence("SECEN"), Sequence("ATCRN"), Sequence("SDCEQ"), Sequence("ASCKN"), Sequence("ATCKQ") ]
 	clusters = []
 
 	for seq in sequences:
