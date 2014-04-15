31868dc4dcbab902e2df8b2cd791d80abf2c1418
Antoine Carpentier <antcarpe@ulb.ac.be>
2014-04-15 16:37:25 +0100

    [add] class Blosum (inherits Score)

--- Blosum.py
+++ Blosum.py
@@ -1,4 +1,5 @@
 from Sequence import Sequence
+from Score import Score
 
 class Cluster(list):
 	def __init__(self, *args):
@@ -11,35 +12,41 @@ class Cluster(list):
 				result += seq1.identity(seq2) >= C
 		return 1-(result/(len(self)*len(other)))
 
+class Blosum(Score):
+	def __init__(self, sequences, threshold):
+		self.threshold = threshold
+		self.clusters = []
+		for seq in sequences:
+			self.clusters.append(Cluster([Sequence(seq)]))
+
+		self.makeClusters()
+		for cluster in self.clusters:
+			print(cluster)
+
+	def makeClusters(self):
+		finished = False
+		while (not finished):
+			minVal = 1
+			finished = True
+			case = None
+			distances = [x[:] for x in [[0]*len(self.clusters)]*len(self.clusters)]
+
+			for i in range(0, len(self.clusters)):
+				for j in range(i+1, len(self.clusters)):
+					value = self.clusters[i].distance(self.clusters[j], self.threshold)
+					distances[i][j] = value
+					if (value <= minVal and value != 1):
+						minVal = value
+						case = (i, j)
+					if(value == 0):
+						finished = False
+
+			if (case):
+				for seq in self.clusters[case[1]]:
+					self.clusters[case[0]].append(seq)
+
+				del self.clusters[case[1]]
+
 if __name__ == '__main__':
 	sequences = [ "TECRQ", "SSCRN", "SECEN", "ATCRN", "SDCEQ", "ASCKN", "ATCKQ" ]
-	clusters = []
-
-	for seq in sequences:
-		clusters.append(Cluster([Sequence(seq)]))
-
-	finished = False
-	while (not finished):
-		minVal = 1
-		finished = True
-		case = None
-		distances = [x[:] for x in [[0]*len(clusters)]*len(clusters)]
-
-		for i in range(0, len(clusters)):
-			for j in range(i+1, len(clusters)):
-				value = clusters[i].distance(clusters[j], 62)
-				distances[i][j] = value
-				if (value <= minVal and value != 1):
-					minVal = value
-					case = (i, j)
-				if(value == 0):
-					finished = False
-
-		if (case):
-			for seq in clusters[case[1]]:
-				clusters[case[0]].append(seq)
-
-			del clusters[case[1]]
-
-	for cluster in clusters:
-		print(cluster)
+	blosum = Blosum(sequences, 62)
