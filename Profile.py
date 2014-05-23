21f2d3fd21e67f8866fe251ce78438b058027238
t00n <kelkununtel@hotmail.com>
2014-05-23 11:03:21 +0200

    [fix] minor stuff in Project3 / cleaned the code

--- Profile.py
+++ Profile.py
@@ -8,38 +8,25 @@ class Profile:
 		self.cluster = Cluster(sequences)
 		self.score = Score.load("scoring-matrices/blosum62.txt")
 		acides = self.score.indexes[:-4]
-		# Nseq = len(self.cluster)
-		pssm = [x[:] for x in [[0]*len(acides)]*len(self.cluster[0])]
-
-		p = [ 8.28, 5.53, 4.05, 5.45, 1.36, 3.94, 6.76, 7.09, 2.27, 5.99, 9.67, 5.85, 2.43, 3.86, 4.68, 6.5, 5.32, 1.07, 2.91, 6.87 ]
-
-		# g = [x[:] for x in [[0]*len(self.cluster[0])]*len(acides)]
-		# for column in range(0, len(self.cluster[0])):
-		# 	for acide in range(0, len(acides)):
-		# 		for b in range(0, len(acides)):
-		# 			fua = self.cluster.getFrequencyInColumn(column, self.score.indexes[acide])
-		# 			fub = self.cluster.getFrequencyInColumn(column, self.score.indexes[b])
-		# 			qab = fua*fub/(len(self.cluster)*(len(self.cluster)-1)*len(self.cluster[0])/2)
-		# 			g[acide][column] += fub*qab/p[b]
-		# for line in g:
-		# 	print(sum(line))
+		Nseq = len(self.cluster)
+		Lseq = len(self.cluster[0])
+		p = {'T': 5.34, 'C': 1.37, 'H': 2.27, 'I': 5.95, 'K': 5.83, 'W': 1.09, 'E': 6.74, 'F': 3.86, 'P': 4.71, 'S': 6.57, 'V': 6.87, 'L': 9.66, 'R': 5.53, 'N': 4.05, 'Q': 3.93, 'G': 7.08, 'M': 2.41, 'D': 5.46, 'A': 8.26, 'Y': 2.92}
+		pssm = [x[:] for x in [[0]*len(acides)]*Lseq]
 
 		# err_rel = 0
-		alpha = len(self.cluster)
-		beta = sqrt(len(self.cluster))
-		for column in range(0, len(self.cluster[0])):
-			for acide in range(0, len(acides)):
+		alpha = Nseq
+		beta = sqrt(Nseq)
+		for column in range(0, Lseq):
+			for acide in acides:
 				pa = p[acide]/100
 				gua = 0
-				for b in range(0, len(acides)):
-					# pb = p[b]/100
-					fub = self.cluster.getFrequencyInColumn(column, self.score.indexes[b])
-					# qab = pa*pb*exp(0.8*self.score.matrix[acide][b])
-					gua += fub*exp(self.score.matrix[acide][b])
+				for b in acides:
+					fub = self.cluster.getFrequencyInColumn(column, b)
+					gua += fub*exp(self.score[acide, b])
 				gua = gua * pa
-				fua = self.cluster.getFrequencyInColumn(column, self.score.indexes[acide])
+				fua = self.cluster.getFrequencyInColumn(column, acide)
 				q = (alpha*fua+beta*gua)/(alpha+beta)
-				pssm[column][acide] = round(log10(q/pa), 3)
+				pssm[column][acides.index(acide)] = round(log10(q/pa), 3)
 				# err_rel += abs(log10(q/pa)-pssm[column][acide])
 		print(acides)
 		for line in pssm:
